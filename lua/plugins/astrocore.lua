893e0eb79f847ff9fdb1fe85daddfb4fa764ebd9
NucleoFusion <lakshit.singh.mail@gmail.com>
2025-03-31 22:25:41 +0530

    theme change, vyfor/cord plugin, and other changes

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -46,6 +44,9 @@ return {
       n = {
         -- second key is the lefthand side of the map
 
+        -- <C-s> Save keybind
+        ["<C-s>"] = { "<cmd>w<CR>" },
+
         -- navigate buffer tabs
         ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
         ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
@@ -66,6 +67,14 @@ return {
 
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
+      },
+
+      i = {
+        ["<C-s>"] = { "<Esc><cmd>w<CR>" },
+      },
+
+      v = {
+        ["<C-s>"] = { "<Esc><cmd>w<CR>" },
       },
     },
   },
